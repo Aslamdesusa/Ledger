a576c32734e8d57f7e71a06e4487386744ffa35b
Aslam <aslam17@navgurukul.org>
2021-04-13 19:40:02 +0530

    complete

--- server.js
+++ server.js
@@ -9,10 +9,11 @@ app.use(cors());
 
 let ENV = process.env.NODE_ENV || 'production';
 
-const config = require(`./${ENV}.config.json`);
-
 app.use(express.static(__dirname + `/temp/${ENV}/`));
 // Handle SPA
 app.get(/.*/, (req, res) => res.sendFile(__dirname + `/temp/${ENV}/index.html`));
 
-app.listen(config.application.port, () => console.log(`Server started on port ${config.application.port}`));
+const port = process.env.PORT || 8080
+app.listen(port)
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
