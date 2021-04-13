f87958c8f49c0b5b5f0541553c308cbf313f67a3
Aslam <aslam17@navgurukul.org>
2021-04-13 21:48:47 +0530

    complete diler

--- src/components/shareBtn.vue
+++ src/components/shareBtn.vue
@@ -12,11 +12,63 @@ export default {
         }
     },
     methods: {
+        localData() {
+            let jData = JSON.parse(localStorage.getItem("JData"));
+            let data = JSON.parse(localStorage.getItem("data"));  
+            let JantriData = [];
+            let finalMessageArray = [];
+            if (jData) {
+                for (let index = 0; index < jData.length; index++) {
+                    const element = jData[index];
+                    for (let index = 0; index < element.length; index++) {
+                        const trunk = element[index];
+                        if (trunk.point && trunk.number != "") {
+                            JantriData.push(trunk);
+                        }
+                    }
+                }
+            }
+            if (data) {
+                JantriData = [...data, ...JantriData];
+            }
+
+            if (JantriData.length) {
+                for (let index = 0; index < JantriData.length; index++) {
+                    const element = JantriData[index];
+                    if (finalMessageArray.length) {
+                        const found = finalMessageArray.find(ele => String(ele.number) == String(element.number));
+                        if (!found) {
+                            finalMessageArray.push(element)
+                        } else {
+                            let foundNumber = Number(found['point'])
+                            foundNumber += Number(element.point)
+
+                            found['point'] = String(foundNumber)
+                        }
+                    } else {
+                        finalMessageArray.push(element)
+                    }
+                }
+            }
+            let message = [];
+            let finalTotal = 0;
+            for (let index = 0; index < finalMessageArray.length; index++) {
+                const element = finalMessageArray[index];
+                let msg = `${element.number}-(${element.point})`
+                finalTotal += Number(element.point)
+                message.push(msg);
+            }
+
+            let finalMessage = message.join() + ` FinalTotal-${finalTotal}`
+
+            return finalMessage
+        },
         shareText() {
+            let shareableMessage = this.localData();
             if (window.navigator.share) {
                 window.navigator.share({
-                    title: 'Total Items',
-                    text: 'Hello World',
+                    title: 'Final Total',
+                    text: shareableMessage,
                 }).then(() =>{
                     console.log('Thanks for sharing!');
                 }).catch(console.error)
